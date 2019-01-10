4b2da104a1ae1ab360eed7f13308dc5ee82115cc
YutaIzumi <izumi.yuuta@gmail.com>
2019-01-10 11:36:50 +0900

    add php and python file

---
+++ phpConnectionTest.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Spyderエディタ
+
+これは一時的なスクリプトファイルです
+"""
+
+import sys
+
+print("1line")
+print("2line")
+print(sys.argv[1])
---
+++ phpPythonTest.php
@@ -0,0 +1,7 @@
+<?php
+$command="phpConnectionTest.py testMessage";
+exec($command, $output);
+print "$output[0]\n";
+print "$output[1]\n";
+print "$output[2]\n";
+?>
