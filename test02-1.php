70e3f7674d169a91ad07a44f71a7f1cb8fc2028d
YutaIzumi <izumi.yuuta@gmail.com>
2019-01-10 15:01:52 +0900

    add test02

---
+++ test02-1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="utf-8">
+</head>
+<body>
+<div>
+    <form method="POST" action="test02-2.php">
+        <ul>
+            <li><label>message:<input type="text" name="message" ></label></li>
+            <li><input type="submit" value="run test02.py" ></li>
+        </ul>
+    </form>
+</div>
+</body>
+</html>
---
+++ test02-2.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="utf-8">
+</head>
+<body>
+<div>
+<?PHP
+    $message = $_POST["message"];
+    $command="test02.py " . $message;
+    exec($command, $output);
+    print "$output[0]\n";
+    print "$output[1]\n";
+    print "$output[2]\n";
+?>
+</div>
+</body>
+</html>
